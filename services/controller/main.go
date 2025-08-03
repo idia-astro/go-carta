7852673372d86da64bd0ff39ad7da2a8c440098d
Angus Comrie <accomrie@gmail.com>
2025-08-03 17:58:23 +0200

    basic grpc test

--- services/controller/main.go
+++ services/controller/main.go
@@ -1,11 +1,51 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"log"
+	"net"
+
 	"github.com/google/uuid"
+	"google.golang.org/grpc"
+
+	pb "idia-astro/go-carta/pkg/grpc"
 )
 
+var (
+	port = flag.Int("port", 8080, "The server port")
+)
+
+// server is used to implement FileServiceServer.
+type server struct {
+	pb.UnimplementedFileServiceServer
+}
+
+// CheckStatus implements FileServiceServer
+func (s *server) CheckStatus(_ context.Context, _ *pb.Empty) (*pb.StatusResponse, error) {
+	log.Printf("Received CheckStatus message")
+	return &pb.StatusResponse{Status: true, StatusMessage: "OK"}, nil
+}
+
 func main() {
+	// Parse arguments and start TCP listener
+	flag.Parse()
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	defer listener.Close()
+
 	id := uuid.New()
-	fmt.Printf("Started controller with UUID: %s\n", id.String())
+	log.Printf("Starting controller with instance ID: %s\n", id.String())
+
+	// Set up gRPC server
+	s := grpc.NewServer()
+	pb.RegisterFileServiceServer(s, &server{})
+	log.Printf("server listening at %v", listener.Addr())
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
 }
--- services/worker/main.go
+++ services/worker/main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"flag"
+	"log"
+	"time"
+
 	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "idia-astro/go-carta/pkg/grpc"
+)
+
+var (
+	remoteAddress = flag.String("remoteAddress", "localhost:8080", "Address of the controller server")
 )
 
 func main() {
 	id := uuid.New()
-	fmt.Printf("Started worker with UUID: %s\n", id.String())
+	log.Printf("Starting worker with UUID: %s\n", id.String())
+
+	flag.Parse()
+	conn, err := grpc.NewClient(*remoteAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Coult not connect to controller: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewFileServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.CheckStatus(ctx, &pb.Empty{})
+	if err != nil {
+		log.Fatalf("could not check controller status: %v", err)
+	}
+	log.Printf("Response from controller: %s", r.StatusMessage)
 }
