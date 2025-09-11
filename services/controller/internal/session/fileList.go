07e7c3cdef5ae59e9a79f673249f4b5732ddafbf
Rob Simmonds <rob@idia.ac.za>
2025-09-11 07:41:37 +0000

    Enable following links.

--- services/controller/internal/session/fileList.go
+++ services/controller/internal/session/fileList.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"strings"
 	"time"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
 
 	"idia-astro/go-carta/pkg/cartaDefinitions"
 	cartaProto "idia-astro/go-carta/pkg/grpc"
@@ -12,11 +16,41 @@ import (
 
 var timeoutDuration = time.Second * 5
 
+// isSymlinkToDir reports whether name in baseDir is a symlink pointing to a directory.
+func isSymlinkToDir(baseDir, name string) bool {
+	full := filepath.Join(baseDir, name)
+
+	fi, err := os.Lstat(full)
+	if err != nil {
+		return false
+	}
+	if fi.Mode()&os.ModeSymlink == 0 {
+		return false
+	}
+
+	target, err := filepath.EvalSymlinks(full)
+	if err != nil {
+		return false
+	}
+
+	tfi, err := os.Stat(target)
+	if err != nil {
+		return false
+	}
+	return tfi.IsDir()
+}
+
+var multiSlash = regexp.MustCompile(`/+`)
+
 func (s *Session) handleFileListRequest(requestId uint32, msg []byte) error {
 	var payload cartaDefinitions.FileListRequest
+
+	log.Printf("Received FileListRequest: %s", string(msg))
+
 	err := s.checkAndParse(&payload, requestId, msg)
 
 	if err != nil {
+		log.Printf("Error parsing FileListRequest: %v", err)
 		return err
 	}
 
@@ -24,10 +58,19 @@ func (s *Session) handleFileListRequest(requestId uint32, msg []byte) error {
 	rpcCtx, cancel := context.WithTimeout(context.Background(), timeoutDuration)
 	defer cancel()
 
+	log.Printf("Handling FileListRequest for path: %s", payload.Directory)
 	path := strings.Replace(payload.Directory, "$BASE", s.BaseFolder, 1)
+	log.Printf("Resolved path: %s", path)
 	if !strings.HasPrefix(path, "/") {
 		path = "/" + path
 	}
+	if !strings.HasSuffix(path, "/") {
+		path = path + "/"
+	}
+
+	path = strings.ReplaceAll(path, "//", "/")
+
+	log.Printf("Fixed path: %s", path)
 
 	rpcResp, err := client.GetFileList(rpcCtx, &cartaProto.FileListRequest{Path: path, IgnoreHidden: true})
 
@@ -44,7 +87,10 @@ func (s *Session) handleFileListRequest(requestId uint32, msg []byte) error {
 	}
 
 	for _, file := range rpcResp.Files {
-		if file.IsDirectory {
+		log.Printf("RPC file type: %s", file.Type)
+		log.Printf("Found file: %s (dir: %v)", file.Name, file.IsDirectory)
+		log.Printf("SYMLINK CHECK: is %s a symlink to dir? %v", file.Name, isSymlinkToDir(path, file.Name))
+		if file.IsDirectory || isSymlinkToDir(path, file.Name) {
 			subDir := cartaDefinitions.DirectoryInfo{
 				Name:      file.Name,
 				ItemCount: 0,
