0f6db5572c4174225972be4f89a7b2e8203ef05f
Rob Simmonds <rob@idia.ac.za>
2025-09-26 05:02:23 +0000

    Clean up.

--- services/controller/internal/session/fileInfo.go
+++ services/controller/internal/session/fileInfo.go
@@ -1,14 +1,12 @@
 package session
 
 import (
-
-
-//	"strings"
-//	"time"
+	"context"
 	"log"
 
 	"idia-astro/go-carta/pkg/cartaDefinitions"
-//	cartaProto "idia-astro/go-carta/pkg/grpc"
+	
+	cartaProto "idia-astro/go-carta/pkg/grpc"
 )
 
 
@@ -18,10 +16,22 @@ func (s *Session) handleFileInfoRequest(requestId uint32, msg []byte) error {
 	log.Printf("Received FileInfoRequest: %s", string(msg))
 
 	err := s.checkAndParse(&payload, requestId, msg)
-
 	if err != nil {
 		log.Printf("Error parsing FileInfoRequest: %v", err)
 		return err
 	}
+	client := cartaProto.NewFileInfoServiceClient(s.WorkerConn)
+	rpcCtx, cancel := context.WithTimeout(context.Background(), timeoutDuration)
+	defer cancel()
+
+	response, err := client.GetFileInfo(rpcCtx, &cartaProto.FileInfoRequest{Path: payload.File})
+	if err != nil {
+		log.Printf("Error getting file info: %v", err)
+		return err
+	}
+
+	rpcCtx = rpcCtx
+	response = response
+
 	return err
 }
