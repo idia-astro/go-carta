b11d5eedba3bc60b749b49c35d6aa0a54cb7cb03
Rob Simmonds <simmonds@cs.uct.ac.za>
2025-12-09 08:18:28 +0200

    Simmonds/serve frontend (#8)
    * Adding serving of carta_frontend. Not working yet.
    
    * Got the serving of carta_frontend working.
    
    * Removed some code that I previously commented out.
    
    * Removed some debug messages.
    
    ---------
    
    Co-authored-by: Rob Simmonds <rob@idia.ac.za>
    Co-authored-by: Angus Comrie <accomrie@gmail.com>

--- services/controller/main.go
+++ services/controller/main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 
-	"github.com/idia-astro/go-carta/pkg/shared"
+	helpers "github.com/idia-astro/go-carta/pkg/shared"
 	"github.com/idia-astro/go-carta/services/controller/internal/session"
 )
 
@@ -21,6 +21,7 @@ var (
 	hostname       = flag.String("hostname", "", "Hostname to listen on")
 	spawnerAddress = flag.String("spawnerAddress", "http://localhost:8080", "Address of the process spawner")
 	baseFolder     = flag.String("baseFolder", "", "Base folder to use")
+	frontendDir    = flag.String("frontendDir", "", "Path to built carta_frontend assets (e.g. /path/to/carta_frontend/build)")
 )
 
 var upgrader = websocket.Upgrader{
@@ -28,6 +29,20 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
+}
+
+// spaHandler serves static files if they exist, otherwise falls back to index.html
+type spaHandler struct {
+	fs http.Handler
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if websocket.IsWebSocketUpgrade(r) {
+		wsHandler(w, r)
+	} else {
+		h.fs.ServeHTTP(w, r)
+	}
+
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
@@ -48,10 +63,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		WebSocket:      c,
 		Context:        subCtx,
 	}
-
 	s.HandleConnection()
-
-	// Close worker on exit if it exists
 	defer s.HandleDisconnect()
 
 	// Basic handler based on gorilla/websocket example
@@ -91,22 +103,36 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	flag.Parse()
-
 	id := uuid.New()
 	log.Printf("Starting controller with UUID: %s\n", id.String())
 
-	// Get
+	// Default baseFolder to $HOME if unset
 	if len(strings.TrimSpace(*baseFolder)) == 0 {
 		dirname, err := os.UserHomeDir()
 		if err != nil {
 			dirname = "/"
 		}
-		err = flag.Set("baseFolder", dirname)
-		if err != nil {
+		if err := flag.Set("baseFolder", dirname); err != nil {
 			log.Fatalf("Failed to set --baseFolder: %v\n", err)
 		}
 	}
 
-	http.HandleFunc("/carta", wsHandler)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", *hostname, *port), nil))
+	// If a frontend directory is provided, serve carta_frontend from there
+	if *frontendDir != "" {
+		info, err := os.Stat(*frontendDir)
+		if err != nil || !info.IsDir() {
+			log.Fatalf("Invalid --frontendDir %q: %v\n", *frontendDir, err)
+		}
+
+		log.Printf("Serving carta_frontend from %s\n", *frontendDir)
+		http.Handle("/", spaHandler{fs: http.FileServer(http.Dir(*frontendDir))})
+	} else {
+		log.Print("No --frontendDir supplied: controller will *not* serve the frontend (only /carta WebSocket).")
+		http.HandleFunc("/carta", wsHandler)
+	}
+
+	addr := fmt.Sprintf("%s:%d", *hostname, *port)
+	log.Printf("Listening on %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+
 }
