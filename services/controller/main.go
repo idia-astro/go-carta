e689da3def049026954f5d54eb23e38935294659
Rob Simmonds <rob@idia.ac.za>
2026-01-07 15:31:20 +0200

    Fixed merge problem.

--- services/controller/main.go
+++ services/controller/main.go
@@ -82,7 +82,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 
 	user, _ := r.Context().Value(session.UserContextKey).(*auth.User)
 
-	s := session.NewSession(r.Context(), c, runtimeSpawnerAddress, runtimeBaseFolder, user)
+	s := session.NewSession(c, runtimeSpawnerAddress, runtimeBaseFolder, user)
 	log.Printf("Created new session for user: %v", user)
 	log.Printf(".   -----   %+v\n", s)
 
