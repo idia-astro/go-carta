5355204845b0edb4df108a8a273c8c8d9126ed3f
Angus Comrie <accomrie@gmail.com>
2026-01-20 18:22:39 +0200

    adjust build process for proto files

--- scripts/build-carta-proto.sh
+++ scripts/build-carta-proto.sh
@@ -9,6 +9,18 @@ fi
 rm -rf pkg/cartaDefinitions
 mkdir -p pkg/cartaDefinitions
 
+# Generate go_opt flags dynamically for each proto file, rather than specifying the go_package option in each source file
+GO_PACKAGE="./cartaDefinitions"
+GO_OPT_FLAGS=""
+for dir in control request shared stream; do
+    for proto_file in proto/carta-protobuf/$dir/*.proto; do
+        if [ -f "$proto_file" ]; then
+            filename=$(basename "$proto_file")
+            GO_OPT_FLAGS="$GO_OPT_FLAGS --go_opt=M$filename=$GO_PACKAGE"
+        fi
+    done
+done
+
 # Generate Go code from proto files
 protoc \
     --proto_path=proto/carta-protobuf/control \
@@ -17,6 +29,7 @@ protoc \
     --proto_path=proto/carta-protobuf/stream \
     --go_out=pkg/cartaDefinitions \
     --go_opt=paths=source_relative \
+    $GO_OPT_FLAGS \
     proto/carta-protobuf/control/*.proto \
     proto/carta-protobuf/request/*.proto \
     proto/carta-protobuf/shared/*.proto \
@@ -29,4 +42,3 @@ if [ $? -ne 0 ]; then
 fi
 
 echo -e "Successfully generated Go code from proto files!"
-echo -e "Generated files are in pkg/carta-protobuf/"
